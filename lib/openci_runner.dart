061d7306597f9677ae60f40483b0119b302d11c3
Masahiro Aoki <freud427@gmail.com>
2023-12-06 11:57:44 +0900

    initial commit

---
+++ lib/openci_runner.dart
@@ -0,0 +1,10 @@
+/// openci_runner, Open CI VM runner
+///
+/// ```sh
+/// # activate openci_runner
+/// dart pub global activate openci_runner
+///
+/// # see usage
+/// openci_runner --help
+/// ```
+library openci_runner;
